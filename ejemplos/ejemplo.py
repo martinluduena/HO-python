e79365d5c53aa215ae114ad21750207bcd27b8fc
Rodrigo Lugones <rodrigolugones@gmail.com>
2018-10-04 22:21:05 -0300

    Consigna agregada. Corregida la resolución.

--- ejemplos/ejemplo.py
+++ ejemplos/ejemplo.py
@@ -1,8 +1,9 @@
+# Si hacemos una lista de todos los números naturales debajo de 10 que sean 
+# múltiplos de 3 o 5 obtendríamos 3, 5, 6 y 9. La suma de los múltiplos es 23.
+# Encuentre la suma de todos los múltiplos de 3 y 5 debajo de 1000.
 
-# scrip to count... and sum them as a list
-
-a= range(1,1001,3) #defined as a list
-b= range(1,1001,5)
-c = set(a) | set(b) #redifined as a set and union
+a = range(0,1000,3) #defined as a list
+b = range(0,1000,5)
+c = set(a) | set(b) #redefined as a set and union
 print c
 print sum(c)
